29a7c02d81f62e735e2a965d85fb2b6bc782fb35
rsantos6 <russellandshira@msn.com>
2017-08-04 13:12:52 -0400

    just some java

--- CreateRun.java
+++ CreateRun.java
@@ -33,17 +33,20 @@ public class CreateRun {
 				getAverage(runList, instructions);
 			} else if (((instructions.contains("show")) || (instructions.contains("print"))) && (instructions.contains("all"))){
 				printAllRuns(runList);
-				
 			} else if (instructions.contains("read")) {
 				runList = scanFile(instructions);
 			} else if (instructions.contains("help")) {
 				printPossibleCommands();
+			} else if ((instructions.contains("morning")) && (instructions.contains("pace")) && (instructions.contains("before"))) {
+				paceBeforeGivenTime(runList, instructions);
 			} else if ((instructions.contains("many")) && (instructions.contains("percent"))) {
 				percentageOnThatDay(runList, instructions);
 			} else if (instructions.contains("many")) {
 				howManyOnThatDay(runList, instructions);
 			} else if ((instructions.contains("start")) && (instructions.contains("morning"))) {
 				averageStartTimeRouter(runList, instructions);
+			} else if ((instructions.contains("start")) && ((instructions.contains("afternoon")) || (instructions.contains("evening")))) {
+				secondAverageStartTimeRouter(runList, instructions);
 			} else {
 				System.out.println("That command is not recognized. Enter 'help' to see all commands" );
 			}
@@ -60,14 +63,17 @@ public class CreateRun {
 		System.out.println("");
 		System.out.println("quit *ends program*");
 		System.out.println("print (show) all *prints all run data*");
+		System.out.println("NOT DEVELOPED YET morning pace before (enter time) *prints average pace before the entered time*");
 		System.out.println("many Monday, Tuesday... *prints how many times you've ran on specific day*");
 		System.out.println("many percent Monday, Tuesday... *prints percentage of time you've ran on specific day*");
 		System.out.println("average (usual) pace *prints average pace over all runs*");
 		System.out.println("average (usual) pace Monday, Tuesday... *prints average pace for that day of the week*");
 		System.out.println("average (usual) distance *prints average distance over all runs*");
 		System.out.println("average (usual) distance Monday, Tuesday... *prints average distance for that day of the week*");
-		System.out.println("start morning *average time that you start your runs in the morning*");
-		System.out.println("start morning Monday, Tuesday... *average time that you start your runs in the morning on specific day*");
+		System.out.println("start morning *prints average time that you start your runs in the morning*");
+		System.out.println("start morning Monday, Tuesday... *prints average time that you start your runs in the morning on specific day*");
+		System.out.println("start afternoon (evening) *prints average time that you start your runs in the afternoon/evening*");
+		System.out.println("start afternoon (evening) Monday, Tuesday... *prints average time that you start your runs in the afternoon/evening on specific day*");
 		System.out.println("");
 	}
 
@@ -282,7 +288,9 @@ public class CreateRun {
 				if(!(runList.get(i).getTime().contains("PM"))) {
 					count ++;
 				String[] hourMin = runList.get(i).getTime().split(":");
-				sumSeconds += Integer.parseInt(hourMin[0]) * 60;
+				if (!(hourMin[0].equals("12"))) {
+					sumSeconds += Integer.parseInt(hourMin[0]) * 60;
+				}
 				if (hourMin[1].substring(0,1).equals("0")){
 					sumSeconds += Integer.parseInt(hourMin[1].substring(1,2));
 				} else {
@@ -297,9 +305,44 @@ public class CreateRun {
 			int seconds = avgSeconds%60;
 
 			if (seconds < 10) {
-				System.out.println("On average you start your runs at " + minutes + ":0" + seconds + " AM");
+				System.out.println("You generally start your runs at " + minutes + ":0" + seconds + " AM");
 			} else {
-				System.out.println("On average you start your runs at " + minutes + ":" + seconds + " AM");
+				System.out.println("You generally start your runs at " + minutes + ":" + seconds + " AM");
+			}
+		}
+	}
+
+	public static void secondAverageStartTimeRouter(ArrayList<Run> runList, String instructions) {
+		instructions = instructions.toLowerCase();
+		if ((instructions.contains("monday") || (instructions.contains("tuesday")) || (instructions.contains("wednesday")) || (instructions.contains("thursday")) || (instructions.contains("friday")) || (instructions.contains("saturday")) || (instructions.contains("sunday"))) && ((instructions.contains("afternoon")) || (instructions.contains("evening")))){
+			averageStartTimeSpecificDayAfternoon(runList, instructions);
+		} else {
+			int count = 0;
+			int sumSeconds = 0;
+			for (int i = 0; i < runList.size(); i++) {
+				if(!(runList.get(i).getTime().contains("AM"))) {
+					count ++;
+				String[] hourMin = runList.get(i).getTime().split(":");
+				if (!(hourMin[0].equals("12"))) {
+					sumSeconds += Integer.parseInt(hourMin[0]) * 60;
+				}
+				if (hourMin[1].substring(0,1).equals("0")){
+					sumSeconds += Integer.parseInt(hourMin[1].substring(1,2));
+				} else {
+					sumSeconds += Integer.parseInt(hourMin[1].substring(0,2));
+				}
+
+				}
+			}
+
+			int avgSeconds = sumSeconds / count;
+			int minutes = avgSeconds/60;
+			int seconds = avgSeconds%60;
+
+			if (seconds < 10) {
+				System.out.println("You generally start your runs at " + minutes + ":0" + seconds + " PM");
+			} else {
+				System.out.println("You generally start your runs at " + minutes + ":" + seconds + " PM");
 			}
 		}
 	}
@@ -312,7 +355,9 @@ public class CreateRun {
 			if((!(runList.get(i).getTime().contains("PM"))) && (runList.get(i).getDay().equals(day))) {
 				count ++;
 			String[] hourMin = runList.get(i).getTime().split(":");
-			sumSeconds += Integer.parseInt(hourMin[0]) * 60;
+			if (!(hourMin[0].equals("12"))) {
+				sumSeconds += Integer.parseInt(hourMin[0]) * 60;
+			}
 			if (hourMin[1].substring(0,1).equals("0")){
 				sumSeconds += Integer.parseInt(hourMin[1].substring(1,2));
 			} else {
@@ -329,7 +374,92 @@ public class CreateRun {
 		if (seconds < 10) {
 			System.out.println("On " + day + "s you generally start your runs at " + minutes + ":0" + seconds + " AM");
 		} else {
-			System.out.println("On " + day + "s you start your runs at " + minutes + ":" + seconds + " AM");
+			System.out.println("On " + day + "s you generally start your runs at " + minutes + ":" + seconds + " AM");
+		}
+	}
+
+	public static void averageStartTimeSpecificDayAfternoon(ArrayList<Run> runList, String instructions) {
+		String day = whichDay(instructions);
+		int count = 0;
+		int sumSeconds = 0;
+		for (int i = 0; i < runList.size(); i++) {
+			if((!(runList.get(i).getTime().contains("AM"))) && (runList.get(i).getDay().equals(day))) {
+				count ++;
+			String[] hourMin = runList.get(i).getTime().split(":");
+			if (!(hourMin[0].equals("12"))) {
+				sumSeconds += Integer.parseInt(hourMin[0]) * 60;
+			}
+			if (hourMin[1].substring(0,1).equals("0")){
+				sumSeconds += Integer.parseInt(hourMin[1].substring(1,2));
+			} else {
+				sumSeconds += Integer.parseInt(hourMin[1].substring(0,2));
+			}
+
+			}
+		}
+
+		int avgSeconds = sumSeconds / count;
+		int minutes = avgSeconds/60;
+		int seconds = avgSeconds%60;
+
+		if (seconds < 10) {
+			System.out.println("On " + day + "s you generally start your runs at " + minutes + ":0" + seconds + " PM");
+		} else {
+			System.out.println("On " + day + "s you generally start your runs at " + minutes + ":" + seconds + " PM");
+		}
+	}
+
+	public static void paceBeforeGivenTime(ArrayList<Run> runList, String instructions) {
+		int index = instructions.indexOf(":");
+		String beforeTime = "";
+		if (instructions.charAt(index-2) == ' '){
+			index --;
+			beforeTime = instructions.substring(index, index+4);
+		} else {
+			index = index - 2;
+			beforeTime = instructions.substring(index, index+5);
+		}
+
+		int count = 0;
+		int sumSeconds = 0;
+		for (int i = 0; i < runList.size(); i++) {
+			if (isBefore(beforeTime, runList.get(i).getTime())) {
+				count ++;
+				String[] hourMin = runList.get(i).getPace().split(":");
+				sumSeconds += Integer.parseInt(hourMin[0]) * 60;
+				if (hourMin[1].substring(0,1).equals("0")){
+					sumSeconds += Integer.parseInt(hourMin[1].substring(1));
+				} else {
+					sumSeconds += Integer.parseInt(hourMin[1]);
+				}
+			}
+				
+		}
+
+		int avgSeconds = sumSeconds / count;
+		int minutes = avgSeconds/60;
+		int seconds = avgSeconds%60;
+
+		if (seconds < 10) {
+			System.out.println("Before " + beforeTime + "AM your pace is usually " + minutes + ":0" + seconds + " per mile");
+		} else {
+			System.out.println("Before " + beforeTime + "AM you pace is usually " + minutes + ":" + seconds + " per mile");
+		}
+
+	}
+
+	public static boolean isBefore(String beforeTime, String runTime){
+		if (runTime.contains("AM")){
+			int index = runTime.indexOf("AM");
+			runTime = runTime.substring(0,index);
+		}
+		
+		if (Integer.parseInt(beforeTime.substring(0,1)) <= Integer.parseInt(runTime.substring(0,1))) {
+			return true;
+		} else if ((Integer.parseInt(runTime.substring(0,1)) == Integer.parseInt(beforeTime.substring(0,1))) && (Integer.parseInt(beforeTime.substring(2)) <= Integer.parseInt(beforeTime.substring(2)))) {
+			return true;
+		} else {
+			return false;
 		}
 	}
 

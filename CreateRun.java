5a88d94db6d98711775e57a2e708007263b4edd0
rsantos6 <russellandshira@msn.com>
2017-08-04 11:24:34 -0400

    nada decir

--- CreateRun.java
+++ CreateRun.java
@@ -1,3 +1,12 @@
+/*
+*Author: Russ Santos
+*Class: CreateRun
+*Purpose: To read, display, and calculate varying aspects of my summer training
+*/
+
+
+
+
 import java.io.*;
 import java.io.File;
 import java.util.UUID;
@@ -29,6 +38,12 @@ public class CreateRun {
 				runList = scanFile(instructions);
 			} else if (instructions.contains("help")) {
 				printPossibleCommands();
+			} else if ((instructions.contains("many")) && (instructions.contains("percent"))) {
+				percentageOnThatDay(runList, instructions);
+			} else if (instructions.contains("many")) {
+				howManyOnThatDay(runList, instructions);
+			} else if ((instructions.contains("start")) && (instructions.contains("morning"))) {
+				averageStartTimeRouter(runList, instructions);
 			} else {
 				System.out.println("That command is not recognized. Enter 'help' to see all commands" );
 			}
@@ -45,10 +60,14 @@ public class CreateRun {
 		System.out.println("");
 		System.out.println("quit *ends program*");
 		System.out.println("print (show) all *prints all run data*");
+		System.out.println("many Monday, Tuesday... *prints how many times you've ran on specific day*");
+		System.out.println("many percent Monday, Tuesday... *prints percentage of time you've ran on specific day*");
 		System.out.println("average (usual) pace *prints average pace over all runs*");
 		System.out.println("average (usual) pace Monday, Tuesday... *prints average pace for that day of the week*");
 		System.out.println("average (usual) distance *prints average distance over all runs*");
 		System.out.println("average (usual) distance Monday, Tuesday... *prints average distance for that day of the week*");
+		System.out.println("start morning *average time that you start your runs in the morning*");
+		System.out.println("start morning Monday, Tuesday... *average time that you start your runs in the morning on specific day*");
 		System.out.println("");
 	}
 
@@ -184,7 +203,8 @@ public class CreateRun {
 			String previous = "something";
 			String temp = console.nextLine();
 			String run = console.nextLine();
-			while ((console.hasNextLine()) && (!run.equals(",,,,"))) {
+			while ((console.hasNextLine()) && (!run.equals(",,,,,"))) {
+				//System.out.println(run);
 				String [] arr = run.split(",");
 				Run newRun = new Run (arr[0], arr[1], arr[2], arr[3]);
 				runList.add(newRun);
@@ -200,6 +220,7 @@ public class CreateRun {
 	}
 
 	public static String whichDay(String instructions) {
+		instructions = instructions.toLowerCase();
 		if (instructions.contains("monday")) {
 			return "Monday";
 		} else if (instructions.contains("tuesday")) {
@@ -215,7 +236,100 @@ public class CreateRun {
 		} else if (instructions.contains("sunday")) {
 			return "Sunday";
 		} else {
-			return null;
+			return "";
+		}
+	}
+
+	public static void howManyOnThatDay(ArrayList<Run> runList, String instructions) {
+		String day = whichDay(instructions);
+		
+		int count = 0;
+		for (int i = 0; i < runList.size(); i++) {
+			if (runList.get(i).getDay().equals(day)) {
+				count ++;
+			}
+		}
+		if (count == 0) {
+			System.out.println("You never run on " + day + "s ");
+		} else {
+			System.out.println("You've ran " + count + " times on " + day);
+		}
+	}
+
+	public static void percentageOnThatDay(ArrayList<Run> runList, String instructions) {
+		String day = whichDay(instructions);
+		
+		double dayCount = 0;
+		double totalCount = 0;
+		for (int i = 0; i < runList.size(); i++) {
+			if (runList.get(i).getDay().equals(day)) {
+				dayCount ++;
+			}
+			totalCount ++;
+		}
+		double percentage = Math.round((dayCount / totalCount) * 100);
+		System.out.println("You run " + percentage + "% of your runs on " + day + "s");
+	}
+
+	public static void averageStartTimeRouter(ArrayList<Run> runList, String instructions) {
+		instructions = instructions.toLowerCase();
+		if ((instructions.contains("monday") || (instructions.contains("tuesday")) || (instructions.contains("wednesday")) || (instructions.contains("thursday")) || (instructions.contains("friday")) || (instructions.contains("saturday")) || (instructions.contains("sunday"))) && (instructions.contains("morning"))){
+			averageStartTimeSpecificDayMorning(runList, instructions);
+		} else {
+			int count = 0;
+			int sumSeconds = 0;
+			for (int i = 0; i < runList.size(); i++) {
+				if(!(runList.get(i).getTime().contains("PM"))) {
+					count ++;
+				String[] hourMin = runList.get(i).getTime().split(":");
+				sumSeconds += Integer.parseInt(hourMin[0]) * 60;
+				if (hourMin[1].substring(0,1).equals("0")){
+					sumSeconds += Integer.parseInt(hourMin[1].substring(1,2));
+				} else {
+					sumSeconds += Integer.parseInt(hourMin[1].substring(0,2));
+				}
+
+				}
+			}
+
+			int avgSeconds = sumSeconds / count;
+			int minutes = avgSeconds/60;
+			int seconds = avgSeconds%60;
+
+			if (seconds < 10) {
+				System.out.println("On average you start your runs at " + minutes + ":0" + seconds + " AM");
+			} else {
+				System.out.println("On average you start your runs at " + minutes + ":" + seconds + " AM");
+			}
+		}
+	}
+
+	public static void averageStartTimeSpecificDayMorning(ArrayList<Run> runList, String instructions) {
+		String day = whichDay(instructions);
+		int count = 0;
+		int sumSeconds = 0;
+		for (int i = 0; i < runList.size(); i++) {
+			if((!(runList.get(i).getTime().contains("PM"))) && (runList.get(i).getDay().equals(day))) {
+				count ++;
+			String[] hourMin = runList.get(i).getTime().split(":");
+			sumSeconds += Integer.parseInt(hourMin[0]) * 60;
+			if (hourMin[1].substring(0,1).equals("0")){
+				sumSeconds += Integer.parseInt(hourMin[1].substring(1,2));
+			} else {
+				sumSeconds += Integer.parseInt(hourMin[1].substring(0,2));
+			}
+
+			}
+		}
+
+		int avgSeconds = sumSeconds / count;
+		int minutes = avgSeconds/60;
+		int seconds = avgSeconds%60;
+
+		if (seconds < 10) {
+			System.out.println("On " + day + "s you generally start your runs at " + minutes + ":0" + seconds + " AM");
+		} else {
+			System.out.println("On " + day + "s you start your runs at " + minutes + ":" + seconds + " AM");
 		}
 	}
 
